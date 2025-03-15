a35fbad9a78abf3cdd857b97f37139dfcefa5342
xthirtysix <ggalliosha@gmail.com>
2025-03-15 19:14:49 +0300

    ⏪️ revert: revert to pnpm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    ssr: false,
     compatibilityDate: '2025-03-12',
     future: {
         compatibilityVersion: 4,
