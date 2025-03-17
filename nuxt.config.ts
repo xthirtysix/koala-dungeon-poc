dd7c27018e51686a6a67289580218ecc5a90f9f8
xthirtysix <ggalliosha@gmail.com>
2025-03-17 07:19:14 +0300

    🏗️ build: prerender artefact images

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,42 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import fs from 'fs'
+import path from 'path'
+
+function getPngFilesFromDirectory(directoryPath) {
+    try {
+        // Проверяем существование директории
+        if (!fs.existsSync(directoryPath)) {
+            console.warn(`Директория ${directoryPath} не существует`)
+            return []
+        }
+
+        // Получаем список всех файлов
+        const files = fs.readdirSync(directoryPath)
+
+        // Фильтруем только PNG файлы
+        return files.filter(
+            (file) => path.extname(file).toLowerCase() === '.png',
+        )
+    } catch (error) {
+        console.error(`Ошибка при чтении директории ${directoryPath}:`, error)
+        return []
+    }
+}
+
+function generateImageRoutes(imageFiles, directory = 'artefacts'): string[] {
+    const routes: string[] = []
+
+    // Для каждого файла создаем маршруты с разными размерами
+    imageFiles.forEach((file) => {
+        routes.push(`/_ipx/q_80&s_300x300/${directory}/${file}`)
+    })
+
+    return routes
+}
+
+const artefactsDir = path.resolve('./public/artefacts')
+const pngFiles = getPngFilesFromDirectory(artefactsDir)
+const imageRoutes = generateImageRoutes(pngFiles)
+
 export default defineNuxtConfig({
     // ssr: false,
     compatibilityDate: '2025-03-12',
@@ -31,6 +69,11 @@ export default defineNuxtConfig({
                     height: 300,
                 },
             },
+        },
+    },
+    nitro: {
+        prerender: {
+            routes: imageRoutes,
         },
     },
 })
